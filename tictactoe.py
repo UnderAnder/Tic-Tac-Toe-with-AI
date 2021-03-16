a6bfe439971ed814dddd0ce8374976dfed153316
UnderAnder <andryushin.alexander@gmail.com>
2021-03-17 00:36:01 +0700

    2/5 Easy does it

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,5 @@
 from re import match
+from random import choice
 
 
 class Game:
@@ -7,7 +8,8 @@ class Game:
     def __init__(self):
         self.menu = Menu()
         self.grid = Grid()
-        self.table_state = dict(zip(self.grid.table, self.menu.init_table_state()))
+        self.ai = AI()
+        self.table_state = self.grid.table
         self.game_state = 'Game not finished'
 
     def start(self):
@@ -16,20 +18,21 @@ class Game:
             self.move()
             self.grid.draw_table(self.table_state)
             self.check_game_state()
-            print(self.game_state)
+
+        print(self.game_state)
 
     def move(self):
         cells = ''.join(self.table_state.values())
-        cell = self.menu.make_move()
+        player = 'X' if cells.count("X") - cells.count("O") == 0 else 'O'
+        cell = self.menu.make_move() if player == 'X' else self.ai.make_move()
 
         if self.table_state[cell] != '_':
             print('This cell is occupied! Choose another one!')
             self.move()
-
-        if cells.count("X") - cells.count("O") == 0:
-            self.table_state[cell] = 'X'
         else:
-            self.table_state[cell] = 'O'
+            if player == 'O':
+                print(f'Making move level "{self.ai.level}"')
+            self.table_state[cell] = player
 
     def check_game_state(self):
         x_pattern = r'(?:X..X..X..|.X..X..X.|..X..X..X|XXX......|...XXX...|......XXX|X...X...X|..X.X.X..)$'
@@ -50,8 +53,21 @@ class Game:
             return
 
 
+class AI:
+    def __init__(self):
+        self.level = 'easy'
+
+    def make_move(self):
+        choices = [digit for digit in range(1, Grid.SIZE + 1)]
+        coord_x = choice(choices)
+        coord_y = choice(choices)
+        return coord_x, coord_y
+
 class Menu:
     def init_table_state(self):
+        """
+        Its not used anymore
+        """
         cells_num = Grid.SIZE ** 2
         command = input('Enter the cells: ').strip()
         if len(command) != cells_num:
@@ -71,7 +87,7 @@ class Menu:
         except ValueError:
             print('You should enter numbers!')
             coord_x, coord_y = self.make_move()
-        if not 0 < coord_x < size + 1 or not 0 < coord_y < size + 1:
+        if not (0 < coord_x < size + 1 and 0 < coord_y < size + 1):
             print(f'Coordinates should be from 1 to {size}!')
             coord_x, coord_y = self.make_move()
         return coord_x, coord_y
@@ -81,11 +97,11 @@ class Grid:
     SIZE = 3
 
     def __init__(self):
-        self.table = [(x, y) for x in range(1, self.SIZE + 1) for y in range(1, self.SIZE + 1)]
+        self.table = {(x, y): '_' for x in range(1, self.SIZE + 1) for y in range(1, self.SIZE + 1)}
 
     @staticmethod
     def draw_table(table_state):
-        values = ''.join(table_state.values())
+        values = ''.join(table_state.values()).replace('_', ' ')
         print("""
             ---------
             | {} {} {} |
